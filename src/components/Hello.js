8cf899a4d68e650df4da4f917cb29d9e67f69cde
tarz2929 <saitarunmulley@gmail.com>
2020-09-10 11:06:45 -0600

    Hello World

---
+++ src/components/Hello.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Hello()
+{
+    return(
+        <h1>
+            Hello, World!
+        </h1>
+    );
+}
+
+// If we want to import our funciton to other files then we have to export here..!
+export default Hello;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Hello from './components/Hello';
+
 
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <></>
+    < Hello/>
   </React.StrictMode>,
   document.getElementById('root')
 );
